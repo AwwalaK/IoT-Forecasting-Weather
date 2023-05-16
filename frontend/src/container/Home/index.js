fdbbae92df6853e1c4dbbdcf40a9fc594333ac56
AwwalaNK <awwalakamila01@gmail.com>
2023-05-16 18:13:04 +0700

    update port and broker

--- frontend/src/container/Home/index.js
+++ frontend/src/container/Home/index.js
@@ -39,16 +39,21 @@ const Home = () =>{
   const [subscribeWeatherNow, setSubscribeWeatherNow] = useState(false)
   const [subscribeWeatherPredict, setSubscribeWeatherPredict] = useState(false)
 
-  const values = {
-    topic: 'outTopic',
+  const baca_data = {
+    topic: 'raw_data',
+    qos: 0,
+  }
+
+  const lihat_prediksi = {
+    topic: 'result_topic',
     qos: 0,
   }
 
   const initialConnectionOptions = {
     protocol: 'ws',
-    host: 'broker.hivemq.com',
+    host: '0.tcp.ap.ngrok.io',
     clientId: 'emqx_react_' + Math.random().toString(16).substring(2, 8),
-    port: 8000,
+    port: 12136,
   }
   const { protocol, host, clientId, port } = initialConnectionOptions
   const url = `${protocol}://${host}:${port}/mqtt`
@@ -60,11 +65,20 @@ const Home = () =>{
     }
   }, [])
 
+
+  const [showElement, setShowElement] = useState(true);
+  useEffect(() => {
+    setTimeout(function () {
+      setShowElement(false);
+    }, 3000);
+  }, []);
+
   useEffect(() => {
     if (client) {
       client.on('connect', () => {
         setConnectStatus('Connected')
-        console.log('connection successful')
+        setShowElement(true)
+        console.log('connection successful web')
       })
 
       client.on('error', (err) => {
@@ -74,6 +88,7 @@ const Home = () =>{
 
       client.on('reconnect', () => {
         setConnectStatus('Reconnecting')
+        setShowElement(true)
       })
 
       client.on('message', (topic, message) => {
@@ -116,19 +131,7 @@ const Home = () =>{
     }
   }
 
-  const mqttUnSub = (subscription) => {
-    if (client) {
-      const { topic, qos } = subscription
-      client.unsubscribe(topic, { qos }, (error) => {
-        if (error) {
-          console.log('Unsubscribe error', error)
-          return
-        }
-        console.log(`unsubscribed topic: ${topic}`)
-      })
-    }
-  }
-
+  console.log(connectStatus)
   return (
     <div className="bg-sunny-background py-8 sm:pt-16 h-full">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -142,13 +145,13 @@ const Home = () =>{
         <div className='border-b-2'>
           <button 
             className='cursor-pointer bg-white rounded-t-lg px-5 py-2 w-60 mr-10'
-            onClick={()=>mqttSub(values, 'now')}
+            onClick={()=>mqttSub(baca_data, 'now')}
           >
             Check Weather Now
           </button>
           <button
             className='cursor-pointer bg-white rounded-t-lg px-5 py-2 w-60'
-            onClick={()=>mqttSub(values, 'predict')}
+            onClick={()=>mqttSub(lihat_prediksi, 'predict')}
           >
             Check Predict Weather
           </button>
@@ -218,6 +221,18 @@ const Home = () =>{
             </div>
           </article>
         )) : ""}
+        
+        {connectStatus === "Connected" && showElement && (
+          <div class="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800 w-60 absolute bottom-0 right-28" role="alert">
+            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg><span class="font-medium">Connection successful</span>
+          </div>
+        )}
+        {connectStatus === "Reconnecting" && showElement && (
+          <div class="flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800 float-right w-60 absolute bottom-0 right-28" role="alert">
+            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+            <span class="font-medium">Reconnecting</span>
+          </div>
+        )}
       </div>
     </div>
   )
